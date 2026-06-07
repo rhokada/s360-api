eca2b313f1daa2df96293c12d9722706ab448491
Rhokada <rhokada@gmail.com>
2026-06-07 20:16:03 -0300

    ambiente

--- portal-supervisao-360/src/environments/environment.prod.ts
+++ portal-supervisao-360/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   //apiUrl: 'https://localhost:44305',
-  apiUrl: 'https://s360-api-desenv.azurewebsites.net/',
+  apiUrl: 'https://s360-api.azurewebsites.net/',
   appId: 'com.bdhelix.s360'
 };
